9203fe742a24107271d1efa94e9a5ff3f1512c32
madanub <madanreact25@gmail.com>
2024-11-04 16:46:59 +0530

    add names

--- reactt/src/Demo.js
+++ reactt/src/Demo.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Demo = () => {
   return (
-    <div>Demo</div>
-  )
+<>
+<h1>JAgAdEEsH</h1>
+<h1>Jayapal</h1>
+<h1>JAgAdEEsH</h1>
+</>  )
 }
 
 export default Demo
