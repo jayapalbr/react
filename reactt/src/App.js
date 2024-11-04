5b2b20a8d30cbf5841f80d0649a21071c9ff9a32
madanub <madanreact25@gmail.com>
2024-11-04 16:51:01 +0530

    add About page

--- reactt/src/App.js
+++ reactt/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Demo from './Demo';
+import { About } from './About';
 
 function App() {
   return (
     <div className="App">
     <h1>jayapal</h1>
     <Demo/>
+    <About/>
     </div>
   );
 }
