4399d499520ab34a690b8aba62a46c92f28615a5
madanub <madanreact25@gmail.com>
2024-11-04 16:42:25 +0530

    add demo page

--- reactt/src/App.js
+++ reactt/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Demo from './Demo';
 
 function App() {
   return (
     <div className="App">
     <h1>jayapal</h1>
+    <Demo/>
     </div>
   );
 }
